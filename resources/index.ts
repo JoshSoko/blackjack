72521c1860040f2ed2989bba9b4ac37886636de1
Joshua <joshuahsokolowski@gmail.com>
2023-09-02 03:25:23 -0400

    First commit

---
+++ resources/index.ts
@@ -0,0 +1,8 @@
+const cards = [2, '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+const suits: { [key: string]: (string | number)[] } = {spades: [...cards], hearts: [...cards], clubs: [...cards], diamonds: [...cards]};
+
+for (let suit in suits) {
+    for (let i=0; i < suits[suit].length; i++){
+        console.log(suits[suit][i] + " of " + suit);
+    }
+}
