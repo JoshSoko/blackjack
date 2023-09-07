c6ad9a5b8c0e60a3c5f4a7c0d3dbaa1275aa8aad
Joshua <joshuahsokolowski@gmail.com>
2023-09-07 19:28:44 -0400

    Fixed start menu

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@
 const cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 const suits = { "spades": [...cards], "hearts": [...cards], "clubs": [...cards], "diamonds": [...cards] };
 let mainMenu = document.getElementById("main-menu");
+let dealer = document.getElementById("dealer");
+let player = document.getElementById("player");
 function startGame() {
     mainMenu.style.display = 'none';
+    player.style.display = 'grid';
+    dealer.style.display = 'grid';
     console.log('Function test');
 }
 /* for (let suit in suits) {
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 const cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 const suits: { [key: string]: string[] } = {"spades": [...cards], "hearts": [...cards],"clubs": [...cards], "diamonds": [...cards]};
 let mainMenu = document.getElementById("main-menu");
+let dealer = document.getElementById("dealer");
+let player = document.getElementById("player");
 
 function startGame() {
     mainMenu!.style.display = 'none';
+    player!.style.display = 'grid';
+    dealer!.style.display = 'grid';
     console.log('Function test');
 }
 
