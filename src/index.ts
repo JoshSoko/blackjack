fa902637bd75a4e4ccaecd04b6b5fc3733b2adbe
Joshua <joshuahsokolowski@gmail.com>
2023-09-13 05:31:27 -0400

    Added randomized card logic

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,45 @@
 const cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
-const suits: { [key: string]: string[] } = {"spades": [...cards], "hearts": [...cards],"clubs": [...cards], "diamonds": [...cards]};
+const suits = ["spades", "hearts", "clubs", "diamonds"]
+const deck: string[] = [];
+
+for (let suit in suits) {
+    for (let card in cards) {
+        deck.push(card + " of " + suits[suit]) ;
+    }
+}
+
 let mainMenu = document.getElementById("main-menu");
 let dealer = document.getElementById("dealer");
 let player = document.getElementById("player");
 
+// Hides menu, displays game, starts a round
 function startGame() {
     mainMenu!.style.display = 'none';
     player!.style.display = 'grid';
     dealer!.style.display = 'grid';
-    console.log('Function test');
+    singleRound();
 }
 
-/* for (let suit in suits) {
-    for (let i=0; i < suits[suit].length; i++){
-        console.log(suits[suit][i] + " of " + suit);
-    }
-} */
+// A new round
+function singleRound() {
+
+    // A new deck is created
+    let roundDeck = [...deck];
+    
+    // The dealer and player are dealt out
+    dealerDeal(roundDeck);
+}
+
+// Deal the dealer out
+function dealerDeal(arr: string[]) {
+
+    // Right now, a card is just generated as a proof of concept
+    let card = cardMath(arr);
+    console.log(card);
+}
+
+
+// Repeatable function to randomize a card
+function cardMath(arr: string[]) {
+    return arr[Math.floor(Math.random() * Number(arr.length) + 1)];
+}
